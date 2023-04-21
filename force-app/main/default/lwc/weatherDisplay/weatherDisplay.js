efbdaf12f00d78c83c02142b35e0a6925c56be85
lauticorradini <corradinilautaro@gmail.com>
2023-04-21 17:01:34 -0300

    basic display of weather

--- force-app/main/default/lwc/weatherDisplay/weatherDisplay.js
+++ force-app/main/default/lwc/weatherDisplay/weatherDisplay.js
@@ -1,21 +1,30 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import fetchWeather from '@salesforce/apex/WeatherController.fetchWeather';
 
 export default class WeatherDisplay extends LightningElement {
     @track city;
+    display;
 
     handleCityChange(event) {
         this.city = event.target.value;
     }
 
-    handleGetWeather() {
-        fetchWeather({city: this.city})
-            .then(result => {
-                console.log('Weather information:', result);
-                //TODO: display weather information on the page
-            })
-            .catch(error => {
-                console.error('Error fetching weather information', error);
-            });
+    @wire(fetchWeather, {city: '$city'})
+    wiredWeather({data, error}){
+        if (data){
+            this.display = {
+                city: data[0].City__c,
+                country: data[0].Country__c,
+                weather: data[0].Name,
+                description: data[0].WeatherDescription__c,
+                temperature: data[0].Temperature__c,
+                humidity: data[0].Humidity__c,
+                wind: data[0].Wind__c,
+                icon: data[0].Icon__c
+            }
+        } else if(error){
+            console.error('Error fetching weather information', error);
+            this.display = undefined;
+        }
     }
 }
